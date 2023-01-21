5e2ba27b4eadac293aa4b9cafdea6aeb03a8cc4d
unnameddeveloper <cryptoforeveralone@gmail.com>
2023-01-22 01:45:08 +0300

    com 8

--- src/services/payment-service.ts
+++ src/services/payment-service.ts
@@ -4,5 +4,6 @@ import { $api } from "../api/api";
 export default class PaymentsService {
     static async createInvoiceLink() {
         return axios.post('https://sneaks-shop-backend.onrender.com/createinvoicelink')
+        // return axios.post('http://localhost:8000/createinvoicelink')
     }
 }
