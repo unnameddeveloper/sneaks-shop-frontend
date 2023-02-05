e4c1b947fd701b7eb20d5ccf66a2815700b1dba7
unnameddeveloper <cryptoforeveralone@gmail.com>
2023-02-05 17:51:30 +0300

    com 62

--- src/components/Item.tsx
+++ src/components/Item.tsx
@@ -10,7 +10,7 @@ const Item = ({product}) => {
   return (
     <>
     <div className="item" data-aos="fade-up" data-aos-duration="600">
-      <Link to={`/item/${product.id}`} onClick={scrollup}><img src={product.images[0]} alt="1" className='image'/></Link>
+      <Link to={`/item/${product.id}`} onClick={(e) => {e.preventDefault(); scrollup()}}><img src={product.images[0]} alt="1" className='image'/></Link>
       <div className="blocknameprice">
         <div className="name">{product.name}</div>
         <div className="price">${product.price}</div>
